fc03ddc60ae0d5dc458b9e5377c38e22fb47d8a7
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2020-11-18 11:29:53 -0700

    Add files via upload

---
+++ models/product_product.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# instruccion para hacer importaciones desde odoo
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class Product_produ(models.Model):
+    _inherit = 'product.product'
+
+    _sql_constraints = [
+        ('default_code_uniq', 'unique(default_code)',
+         "A default code can only be assigned to one product !"),
+    ]
