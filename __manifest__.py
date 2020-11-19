34cb64c8bde4e963f4ca368fdff52761ac14f4df
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2020-11-18 17:32:26 -0700

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Campo Unico',
-    'version': '13.0.0.5',
+    'version': '13.0.0.6',
     'author': 'Demsa',
     'website': '',
     'depends': [
         'stock',
+        'hr',
 
     ],
     'data': [
