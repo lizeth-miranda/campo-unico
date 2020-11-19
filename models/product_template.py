55e1c836220bc71853ae0962aa3ce3be284b6d28
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2020-11-19 12:51:29 -0700

    Update product_template.py

--- models/product_template.py
+++ models/product_template.py
@@ -2,10 +2,15 @@
 # instruccion para hacer importaciones desde odoo
 from odoo import fields, models
 
+
 class product_temple(models.Model):
     _inherit = 'product.template'
 
+    campo_unico = fields.Char(
+        string="Campo Unico",
+    )
+
     _sql_constraints = [
-        ('default_code_uniq', 'unique(default_code)',
+        ('campo_unico_uniq', 'unique(campo_unico)',
          "A default code can only be assigned to one product !"),
     ]
